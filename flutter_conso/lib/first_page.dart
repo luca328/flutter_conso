7c35cd49d03a9f7584fbc79a82741ac3ec20b621
Luca83 <luca.demartini@hotmail.fr>
2023-10-23 15:47:36 +0200

    dropdown menu + liste des département

--- flutter_conso/lib/first_page.dart
+++ flutter_conso/lib/first_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:flutter_conso/helpers/departement.dart';
+
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
@@ -14,6 +16,8 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   late List<dynamic> data = []; //Stocker les données de l'API
   Set<String> displayedCommunes = Set<String>();
+  final TextEditingController departementController = TextEditingController();
+  TextLabel? selectedDepartement;
 
   @override
   void initState() {
@@ -23,7 +27,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   Future<void> fetchDataFromAPI() async {
     final url = Uri.parse(
-      'https://enedis.opendatasoft.com/api/v2/catalog/datasets/consommation-annuelle-residentielle-par-adresse/records',
+      "https://enedis.opendatasoft.com/api/v2/catalog/datasets/consommation-annuelle-residentielle-par-adresse/records",
     );
 
     final response = await http.get(url);
@@ -41,25 +45,51 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    final List<DropdownMenuEntry<TextLabel>> departements = <DropdownMenuEntry<TextLabel>>[];
+    for (final departement in TextLabel.values) {
+      departements.add(DropdownMenuEntry<TextLabel>(
+        value: departement,
+        label: departement.label
+      ));
+    }
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: ListView.builder(
-        itemCount: data.length,
-        itemBuilder: (context, index) {
-           final commune = data[index]['record']['fields']['nom_commune'];
-          if (!displayedCommunes.contains(commune)) {
-            displayedCommunes.add(commune);
-            return ListTile(
-              title: Text(commune),
-              subtitle: Text('Consommation : ${data[index]['record']['fields']['consommation_annuelle_moyenne_de_la_commune_mwh']} MWh'),
-            );
-          } else {
-            return Container();
-          }
-        },
-      ),
+      body: Column(
+        children: [
+          DropdownMenu<TextLabel>(
+            initialSelection: TextLabel.ain,
+            controller: departementController,
+            label: const Text('Département'),
+            dropdownMenuEntries: departements,
+            onSelected: (TextLabel? label){
+              setState(() {
+                selectedDepartement = label;
+              });
+            },
+          ),
+          Text('vous avez sélectionné : ${selectedDepartement?.label??TextLabel.ain.label}'),
+          SizedBox(
+            height: 200,
+            child: ListView.builder(
+              itemCount: data.length,
+              itemBuilder: (context, index) {
+                final commune = data[index]['record']['fields']['nom_commune'];
+                if (!displayedCommunes.contains(commune)) {
+                  displayedCommunes.add(commune);
+                  return ListTile(
+                    title: Text(commune),
+                    subtitle: Text('Consommation : ${data[index]['record']['fields']['consommation_annuelle_moyenne_de_la_commune_mwh']} MWh'),
+                  );
+                } else {
+                  return Container();
+                }
+              },
+            ),
+          ),
+        ],
+      )
     );
   }
 }

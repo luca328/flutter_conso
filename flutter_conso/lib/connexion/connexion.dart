aeeeb1889ec459cf6e47639741b9edec25f0e6f8
Luca83 <luca.demartini@hotmail.fr>
2023-10-28 18:32:02 +0200

    fix login page and sign up page

--- flutter_conso/lib/connexion/connexion.dart
+++ flutter_conso/lib/connexion/connexion.dart
@@ -1,9 +1,7 @@
 import 'package:mongo_dart/mongo_dart.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_bcrypt/flutter_bcrypt.dart';
 
 
-Future<void> openMongoDB() async {
+openMongoDB() async {
   final db = Db('mongodb+srv://luludemartini:SofczhCLU9UthMzp@cluster0.36xxfbd.mongodb.net/?retryWrites=true&w=majority<');
-  await db.open();
+  return db;
 }
--- flutter_conso/lib/signupPage.dart
+++ flutter_conso/lib/signupPage.dart
@@ -1,52 +1,105 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bcrypt/flutter_bcrypt.dart';
+import 'package:flutter_conso/connexion/connexion.dart';
 
 class SignupPage extends StatelessWidget {
-  const SignupPage({super.key});
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController residenceController = TextEditingController();
+
+  void createUser() async {
+
+    final String email = emailController.text;
+    final String password = passwordController.text;
+    final String residence = residenceController.text;
+
+    final hashedPassword = await FlutterBcrypt.hashPw(password: password, salt: await FlutterBcrypt.salt());
+
+    var db = openMongoDB();
+
+    try {
+      await db.open();
+
+      final userCollection = db.collection('utilisateurs');
+
+      final userDocument = {
+        'email': email,
+        'password': hashedPassword,
+        'residence': residence,
+      };
+
+      await userCollection.save(userDocument);
+
+      await db.close();
+
+      var context;
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('Compte créé'),
+            content: const Text('Votre compte a été créé avec succès.'),
+            actions: <Widget>[
+              TextButton(
+                child: const Text('OK'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        },
+      );
+    } catch (e) {
+      print('Erreur lors de la création du compte : $e');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Créer un compte'),
+        title: const Text('Créer un compte'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            // Champ Email
-            const Padding(
-              padding: EdgeInsets.all(10.0),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
               child: TextField(
-                decoration: InputDecoration(
+                controller: emailController,
+                decoration: const InputDecoration(
                   labelText: 'Email',
                 ),
               ),
             ),
 
-
-            const Padding(
-              padding: EdgeInsets.all(10.0),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
               child: TextField(
-                decoration: InputDecoration(
+                controller: passwordController,
+                decoration: const InputDecoration(
                   labelText: 'Mot de passe',
                 ),
                 obscureText: true,
               ),
             ),
 
-            const Padding(
-              padding: EdgeInsets.all(10.0),
+            // Champ Lieu de Résidence
+            Padding(
+              padding: const EdgeInsets.all(10.0),
               child: TextField(
-                decoration: InputDecoration(
+                controller: residenceController,
+                decoration: const InputDecoration(
                   labelText: 'Lieu de résidence',
                 ),
               ),
             ),
 
+            // Bouton de Création de Compte
             ElevatedButton(
-              onPressed: () {
-                // Gérer la création de compte ici
-              },
+              onPressed: createUser,
               child: const Text('Créer un compte'),
             ),
           ],
